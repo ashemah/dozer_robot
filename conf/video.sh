ff0e9cd27048fac2c8b1cd6c6ad7e59396cc6cd3
Ashemah Harrison <ashemah@boostedhuman.com>
2015-11-21 19:11:58 +1100

    Video script

---
+++ conf/video.sh
@@ -0,0 +1,1 @@
+LD_LIBRARY_PATH=/home/pi/work/mjpg-streamer-master/mjpg-streamer-experimental /home/pi/work/mjpg-streamer-master/mjpg-streamer-experimental/mjpg_streamer -o "output_http.so -w /usr/share/nginx/www --port 8081" -i "input_raspicam.so -x 320 -y 240 -rot 180 -fps 30 -quality 30"
